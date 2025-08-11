d17b3ee815ac2ecd0cfdf7679345bf404baddff1
TimurKuvvatov <tkuvvatov@gmail.com>
2025-08-11 18:10:26 +0600

    feat(paint-page): integrate Toolbar with Canvas layout

--- frontend/src/pages/paint/ui/PaintPage.tsx
+++ frontend/src/pages/paint/ui/PaintPage.tsx
@@ -1,13 +1,29 @@
-import { Stack } from '@mui/material';
+import { Layout } from 'antd';
 
 import { Canvas } from '@/features/drawing';
+import { ToolBar } from '@/features/toolbar/ui/ToolBar';
 
 export const PaintPage = () => (
-	<Stack
-		alignItems='center'
-		justifyContent='center'
-		width='100vw'
-		height='100vh'>
-		<Canvas />
-	</Stack>
+	<Layout
+		style={{
+			height: '100vh',
+			width: '100vw',
+			backgroundColor: '#8c00ffff',
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center'
+		}}>
+		<Layout
+			style={{
+				display: 'flex',
+				flexDirection: 'row',
+				justifyContent: 'space-between',
+				background: 'none',
+				width: '100%',
+				padding: '10px 100px'
+			}}>
+			<ToolBar />
+			<Canvas />
+		</Layout>
+	</Layout>
 );
