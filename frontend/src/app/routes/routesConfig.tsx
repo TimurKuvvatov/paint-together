1d8fbb00427a1963f0cb3b87216cf912749a0c9f
TimurKuvvatov <tkuvvatov@gmail.com>
2025-08-24 21:21:45 +0600

    feat(frontend/routes): add route for HomePage

--- frontend/src/app/routes/routesConfig.tsx
+++ frontend/src/app/routes/routesConfig.tsx
@@ -1,3 +1,7 @@
+import { HomePage } from '@/pages/home/ui/HomePage';
 import { PaintPage } from '@/pages/paint';
 
-export const routes = [{ path: '/', element: <PaintPage /> }];
+export const routes = [
+	{ path: '/', element: <HomePage /> },
+	{ path: '/room/:roomId', element: <PaintPage /> }
+];
