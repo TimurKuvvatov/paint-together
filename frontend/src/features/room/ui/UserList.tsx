20379becf9c85f52fd29dbde93042591ba9579f4
TimurKuvvatov <tkuvvatov@gmail.com>
2025-08-31 20:57:59 +0600

    refactor: replace UserList props with direct $room store connection

--- frontend/src/features/room/ui/UserList.tsx
+++ frontend/src/features/room/ui/UserList.tsx
@@ -1,35 +1,36 @@
 import { StarFilled, UserOutlined } from '@ant-design/icons';
 import { Card, Space, Typography } from 'antd';
+import { useUnit } from 'effector-react';
 
-import type { User } from '../types/entities';
+import { $room } from '../model/store';
 
-type UserListProps = {
-	users: User[];
+const UserList = () => {
+	const { users } = useUnit($room);
+
+	return (
+		<Space
+			direction='vertical'
+			size='small'>
+			{users.map(user => (
+				<Card
+					key={user.id}
+					size='small'
+					style={{
+						borderColor: user.role === 'host' ? 'gold' : undefined,
+						borderWidth: '2px'
+					}}>
+					<Typography>
+						{user.role === 'host' ? (
+							<StarFilled style={{ marginRight: '5px' }} />
+						) : (
+							<UserOutlined style={{ marginRight: '5px' }} />
+						)}
+						{user.nickname}
+					</Typography>
+				</Card>
+			))}
+		</Space>
+	);
 };
-
-const UserList = ({ users }: UserListProps) => (
-	<Space
-		direction='vertical'
-		size='small'>
-		{users.map(user => (
-			<Card
-				key={user.id}
-				size='small'
-				style={{
-					borderColor: user.role === 'host' ? 'gold' : undefined,
-					borderWidth: '2px'
-				}}>
-				<Typography>
-					{user.role === 'host' ? (
-						<StarFilled style={{ marginRight: '5px' }} />
-					) : (
-						<UserOutlined style={{ marginRight: '5px' }} />
-					)}
-					{user.nickname}
-				</Typography>
-			</Card>
-		))}
-	</Space>
-);
 
 export default UserList;
