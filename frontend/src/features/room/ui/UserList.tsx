c3db6a271a37d0b589f9c2194361a4ff7907160f
TimurKuvvatov <tkuvvatov@gmail.com>
2025-09-13 19:16:27 +0600

    feat(frontend): redesign user list

--- frontend/src/features/room/ui/UserList.tsx
+++ frontend/src/features/room/ui/UserList.tsx
@@ -1,5 +1,5 @@
 import { StarFilled, UserOutlined } from '@ant-design/icons';
-import { Card, Space, Typography } from 'antd';
+import { Card, Flex, Typography } from 'antd';
 import { useUnit } from 'effector-react';
 
 import { $room } from '../model/store';
@@ -8,28 +8,56 @@ const UserList = () => {
 	const { users } = useUnit($room);
 
 	return (
-		<Space
-			direction='vertical'
-			size='small'>
+		<Flex
+			style={{
+				width: '200px',
+				background: '#fff',
+				flexDirection: 'column',
+				alignSelf: 'start',
+				gap: '10px',
+				padding: '5px',
+				borderRadius: '10px',
+				maxHeight: '90%',
+				overflowY: 'auto'
+			}}>
+			<Typography.Title
+				level={2}
+				style={{ textAlign: 'center', margin: 0 }}>
+				Artists
+			</Typography.Title>
 			{users.map(user => (
 				<Card
 					key={user.id}
 					size='small'
 					style={{
-						borderColor: user.role === 'host' ? 'gold' : undefined,
+						borderColor: user.role === 'host' ? 'gold' : '#bcbcbc',
 						borderWidth: '2px'
 					}}>
-					<Typography>
-						{user.role === 'host' ? (
-							<StarFilled style={{ marginRight: '5px' }} />
-						) : (
-							<UserOutlined style={{ marginRight: '5px' }} />
-						)}
-						{user.nickname}
-					</Typography>
+					<Flex
+						gap={5}
+						align='center'
+						style={{ alignItems: 'center' }}>
+						<span
+							style={{
+								width: '18px',
+								height: '18px',
+								border: '1px solid #bcbcbc',
+								display: 'inline-flex',
+								alignItems: 'center',
+								justifyContent: 'center',
+								borderRadius: '50%'
+							}}>
+							{user.role === 'host' ? (
+								<StarFilled style={{ color: 'gold' }} />
+							) : (
+								<UserOutlined style={{ color: '#bcbcbc' }} />
+							)}
+						</span>
+						<Typography.Text>{user.nickname}</Typography.Text>
+					</Flex>
 				</Card>
 			))}
-		</Space>
+		</Flex>
 	);
 };
 
